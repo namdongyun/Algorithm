4d50134c64dbde5752aa0fc98c0c25a39195a1d9
ndy <dynam99@naver.com>
2023-12-05 04:26:53 +0900

    [Bronze III] Title: 윷놀이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.py
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -1,20 +1,4 @@
-numArray1 = list(map(int, input().split()))
-numArray2 = list(map(int, input().split()))
-numArray3 = list(map(int, input().split()))
+res = ['D', 'C', 'B', 'A', 'E']
 
-allArray = []
-allArray.append(numArray1)
-allArray.append(numArray2)
-allArray.append(numArray3)
-
-for i in allArray:
-    if i.count(0) == 0:
-        print("E")
-    elif i.count(0) == 1:
-        print("A") 
-    elif i.count(0) == 2:
-        print("B")
-    elif i.count(0) == 3:
-        print("C")
-    elif i.count(0) == 4:
-        print("D")
+for _ in range(3):
+    print(res[sum(list(map(int, input().split())))])
