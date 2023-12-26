5a99c9948ec9c0df2d3aed8ea3a92fda05259a34
ndy <dynam99@naver.com>
2023-12-26 18:10:00 +0900

    Update 스택 수열.py

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -11,27 +11,25 @@ current = 0
 for i in range(n):
     num = int(input())
 
-    while current < num:
+    while current < num:    # 입력한 num 이 current 보다 클 경우
         current += 1
-        stack.append(current)
-        result.append('+')
+        stack.append(current)   # current 값을 1씩 올리며 stack 에 넣어줌
+        result.append('+')      # push 이므로 +
 
-        if current == num:
-            stack.pop()
-            result.append('-')
-            continue
+        if current == num:      # current 와 num 이 같으면
+            stack.pop()         # pop 으로 숫자를 빼냄
+            result.append('-')  # pop 이므로 -
 
-
-    if current > num:
+    if current > num:   # 입력한 num 이 current 보다 작을 경우
 
         if stack[-1] == num:
             stack.pop()
             result.append('-')
-        elif stack[-1] != num:
-            result.append(-1)
+        elif stack[-1] != num:  # 스택의 마지막 숫자가 입력한 num이 아니면 
+            result.append(-1)   # result 의 마지막 인덱스에 -1을 추가
             break
 
-if result[-1] == -1:
+if result[-1] == -1:    # result 의 마지막 인덱스가 -1이면 NO 출력
     print('NO')
 else:
     for i in range(len(result)):
