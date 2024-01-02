d780d25aa4eca3f7a5083732154dfaabd0ce208d
ndy <dynam99@naver.com>
2024-01-03 01:41:11 +0900

    Update 회전하는 큐.py

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -17,12 +17,12 @@ for i in positionList:
             dq.popleft()
             break
         else:
-            if dq.index(i) <= len(dq)/2:
-                while dq[0] != i:
+            if dq.index(i) <= len(dq)/2:    # dq의 크기를 반으로 나누어 현재 찾는 i값이 있는 dq의 인덱스 위치와 비교함
+                while dq[0] != i:           # dq에 있는 i값의 인덱스가 dq 크기보다 작거나 같으면 맨 앞 숫자를 뒤로 넘겨 i값을 구함
                     dq.rotate(-1)
                     count += 1
             else:
-                while dq[0] != i:
+                while dq[0] != i:           # dq에 있는 i값의 인덱스가 dq 크기보다 크면 맨 뒤 숫자를 앞으로 넘겨서 i값을 구함
                     dq.rotate(1)
                     count += 1
 print(count)
