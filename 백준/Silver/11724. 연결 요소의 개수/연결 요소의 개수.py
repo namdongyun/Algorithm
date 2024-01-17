843665942f9b9fafbf1a258018de39291583af47
ndy <dynam99@naver.com>
2024-01-18 01:44:33 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 676 ms, Memory: 65192 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -17,19 +17,15 @@ count = 0
 
 
 def dfs(x):
-    if not graph[x] or visited[x]:  # graph[x]와 연결된 정점이 없거나, 이미 방문 했을 경우 False
-        return False
-
     visited[x] = True   # 방문 표시
     for i in graph[x]:  # graph[x]와 연결된 정점 모두를 탐색
-        dfs(i)
-
-    return True
+        if not visited[i]:
+            dfs(i)
 
 
 for j in range(1, N+1):
     if not visited[j]:  # 방문 하지 않았을 경우
-        if not graph[j]:  # graph[j] 와 연결된 정점이 없는 경우
+        if not graph[j]:  # graph[j] 와 연결된 정점이 없는 경우도 count 올림
             visited[j] = True
             count += 1
         else:
