6d66a9b61b52d29b70480da02868a6267281fb71
ndy <dynam99@naver.com>
2024-01-20 04:11:54 +0900

    [Silver I] Title: 나이트의 이동, Time: 1328 ms, Memory: 34088 KB -BaekjoonHub

---
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -0,0 +1,47 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+testCase = int(input())
+
+dy = [1, 1, 2, 2, -1, -1, -2, -2]
+dx = [2, -2, 1, -1, 2, -2, 1, -1]
+
+
+def bfs(cPoY, cPoX):
+    dq = deque()
+    dq.append((cPoY, cPoX))     # 나이트의 현재 위치 넣어줌
+
+    while dq:
+        y, x = dq.popleft()     # 나이트의 현재 위치 추출
+
+        for i in range(8):
+            ny = y + dy[i]
+            nx = x + dx[i]
+
+            if 0 <= ny < l and 0 <= nx < l and not graph[ny][nx]:
+                graph[ny][nx] = graph[y][x] + 1     # 나이트가 이동하는 지점마다 이전 좌표의 값 + 1
+                dq.append((ny, nx))
+
+                if ny == dPoY and nx == dPoX:
+                    return graph[ny][nx]
+        
+
+
+for _ in range(testCase):
+    l = int(input())    # 한 변의 길이
+    cPoY, cPoX = map(int, input().split())  # 나이트가 현재 있는 칸
+    dPoY, dPoX = map(int, input().split())  # 나이트가 이동하려고 하는 칸
+
+    if cPoY == dPoY and cPoX == dPoX:
+        print(0)
+        continue
+
+    graph = [[0 for _ in range(l)] for _ in range(l)]
+    graph[cPoY][cPoX] = 1   # 나이트의 현재 위치 좌표에 1값을 넣음
+
+    print(bfs(cPoY, cPoX) - 1)
+
+
+
