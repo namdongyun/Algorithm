33e67dc83187cf9ded7e3c4475cd736a73e7b6e6
ndy <dynam99@naver.com>
2024-01-28 20:20:42 +0900

    [Silver III] Title: N과 M (2), Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())    # 1~N까지 중복 없이 M개를 고른 수열
+s = []
+
+def dfs(start):
+    if len(s) == M:
+        print(' '.join(map(str, s)))
+        return
+
+    for i in range(start, N+1):
+        if i not in s:
+            s.append(i)
+            dfs(i+1)
+            s.pop()
+
+dfs(1)
+
