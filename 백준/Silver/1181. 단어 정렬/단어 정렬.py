b8ee0fac020a2043900308e7ccbfaa4a795de53d
ndy <dynam99@naver.com>
2024-03-05 14:58:01 +0900

    [Silver V] Title: 단어 정렬, Time: 80 ms, Memory: 36396 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -3,13 +3,8 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-arr = []
+arr = set([input().rstrip() for _ in range(N)])
 
-for _ in range(N):
-    row = input().rstrip()
-    if row not in arr:  # arr에 row와 같은 중복된 단어가 없으면 append
-        arr.append(row)
-
-arr.sort(key=lambda x:(len(x), x))
+arr = sorted(arr, key=lambda x:(len(x), x))
 
 print('\n'.join(map(str, arr)))
