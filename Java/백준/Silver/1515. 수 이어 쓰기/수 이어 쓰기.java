6e563d19a50e087a34e899f503ea302ec378ae06
ndy <dynam99@naver.com>
2025-04-22 17:56:25 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 124 ms, Memory: 16252 KB -BaekjoonHub

--- Java/백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
+++ Java/백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -1,11 +1,7 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) throws IOException{
